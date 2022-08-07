688bedf4704fbc907c46a0840e5ed00ed0dd1c5f
abhayshuklainfo9 <abhayshuklainfo9@gmail.com>
2022-08-07 12:13:21 +0530

    GUI Based marksheet

---
+++ basic.py
@@ -0,0 +1,25 @@
+from ast import Num
+
+
+# for loop
+# fruits=["lemon","achar","chamach"]
+# for x in fruits:
+#     print(x)
+
+# # for loop with control and break statement
+# fruits=["mqango","anar","jamun","papaya"]
+# for x in fruits:
+#     print(x)
+#     if x=="jamun":
+#      break
+
+#for loop for continue sttaemnt
+fruits=["apple","mango","banana"]
+for x in fruits:
+    if x=="mango":
+        continue
+    print(x)
+
+#for nested looping
+adj=["Red","big","tasty"]
+fruit=["del"]
